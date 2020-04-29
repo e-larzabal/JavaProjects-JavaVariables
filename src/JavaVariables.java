87c6870b49f639164225134605d8de3b0509af65
Emmanuelle LARZABAL <emmanuelle.larzabal64@gmail.com>
2020-04-29 15:27:21 +0200

    add .classpath .project JavaVariables.class JavaVariables.java

---
+++ src/JavaVariables.java
@@ -0,0 +1,16 @@
+
+public class JavaVariables {
+	
+	
+	public static void main(String[] args){ 
+		String sTitle = "Indiana Jones et la dernière croisage"; 
+		boolean bViewed = false;
+		int iReleaseDate = 1989;
+		float fMark = 8.2f;
+
+		System.out.println(sTitle); 
+		System.out.println(bViewed); 
+		System.out.println(iReleaseDate); 
+		System.out.println(fMark); 
+	 } 
+}
